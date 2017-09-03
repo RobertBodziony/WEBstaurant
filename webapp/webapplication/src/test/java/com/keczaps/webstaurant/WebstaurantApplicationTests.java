5153bc031296fe83cf58b2eca66f4ea79c9800ca
Keczaps <robertbodziony93@gmail.com>
2017-09-03 18:16:23 +0200

    Initial repository - First commit

---
+++ webapp/webapplication/src/test/java/com/keczaps/webstaurant/WebstaurantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keczaps.webstaurant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebstaurantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
